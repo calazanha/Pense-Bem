d19bdfc932af5517165e8ef72e7182134fb882fa
Felipe Calazans do Prado Carvalho <felipe.carvalho@lasalle>
2025-05-23 11:51:30 -0300

    Incluindo perguntas e botões sobre harry potter e starwars

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,5 +1,11 @@
 import './styles/StartScreen.css';
 
+import { TbHelmet } from "react-icons/tb";
+import { CgPokemon } from "react-icons/cg";
+import { PiSoccerBallLight } from "react-icons/pi";
+import { GiMagicBroom } from "react-icons/gi";
+import { GiLightSabers } from "react-icons/gi";
+
 const StartScreen = ({ onStart, selectedProgram, setSelectedProgram }) => {
   const selectProgram = (programNumber) => {
     setSelectedProgram(programNumber);
@@ -7,7 +13,7 @@ const StartScreen = ({ onStart, selectedProgram, setSelectedProgram }) => {
 
   return (
     <div className="start-screen">
-      <h1>PENSE BEM</h1>
+      <h1>PENSE BEM - SENNINHA</h1>
       
       <div className="device-container">
         <div className="device-screen">
@@ -20,31 +26,31 @@ const StartScreen = ({ onStart, selectedProgram, setSelectedProgram }) => {
             className={`program-button ${selectedProgram === 1 ? 'active' : ''}`}
             onClick={() => selectProgram(1)}
           >
-            1
+            <TbHelmet />
           </button>
           <button 
             className={`program-button ${selectedProgram === 2 ? 'active' : ''}`}
             onClick={() => selectProgram(2)}
           >
-            2
+            <CgPokemon />
           </button>
           <button 
             className={`program-button ${selectedProgram === 3 ? 'active' : ''}`}
             onClick={() => selectProgram(3)}
           >
-            3
+            <PiSoccerBallLight />
           </button>
           <button 
             className={`program-button ${selectedProgram === 4 ? 'active' : ''}`}
             onClick={() => selectProgram(4)}
           >
-            4
+            <GiMagicBroom />
           </button>
           <button 
             className={`program-button ${selectedProgram === 5 ? 'active' : ''}`}
             onClick={() => selectProgram(5)}
           >
-            5
+            <GiLightSabers />
           </button>
         </div>
         
